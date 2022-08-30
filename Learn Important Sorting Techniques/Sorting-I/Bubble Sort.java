bb64bb78947d4dc38bee09e23bea7158ea5da54a
Astha Negi <asthanegi539@gmail.com>
2022-08-30 19:40:13 +0530

    Create Bubble Sort.java

---
+++ Learn Important Sorting Techniques/Sorting-I/Bubble Sort.java
@@ -0,0 +1,17 @@
+class Solution
+{
+    //Function to sort the array using bubble sort algorithm.
+	public static void bubbleSort(int arr[], int n)
+    {
+        //code here
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] =temp;
+                }
+            }
+        }
+    }
+}
