3e38a649f8e23cc9a7133f873e943f28e9a13494
Astha Negi <asthanegi539@gmail.com>
2023-02-02 21:33:27 +0530

    Create Implement stack using array.java

---
+++ Stack and Queues [Pre-In-Post-fix, Monotonic Stack]/Learning/Implement stack using array.java
@@ -0,0 +1,35 @@
+class MyStack
+{
+    int top;
+	int arr[] = new int[1000];
+
+    MyStack()
+	{
+		top = -1;
+	}
+	
+	//Function to push an integer into the stack.
+    void push(int a)
+	{
+	    // Your code here
+	    
+	    if(top == arr.length-1){
+	        return;
+	    }
+	    arr[++top]=a;
+	} 
+	
+    //Function to remove an item from top of the stack.
+	int pop()
+	{
+        // Your code here
+        if(top<=-1){
+            return -1;
+        }
+        
+        int pop = arr[top];
+        top--;
+        
+        return pop; 
+	}
+}
